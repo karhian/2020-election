e6283b1eb0d822cb84d19eedfe8d5d00a1904b70
Fadlan Arif Fakhrunniam <fadlan.fakhrunniam@mail.utoronto.ca>
2020-10-28 12:32:35 +0800

    cleaned ipums data

--- scripts/01-data_cleaning-post-strat.R
+++ scripts/01-data_cleaning-post-strat.R
@@ -10,11 +10,11 @@
 
 
 #### Workspace setup ####
+library(math)
 library(haven)
 library(tidyverse)
 # Read in the raw data. 
-raw_data <- read_dta("inputs/data/usa_00003.dta"
-                     )
+raw_data <- read_dta("data/usa_00001.dta.gz")
 # Add the labels
 raw_data <- labelled::to_factor(raw_data)
 
@@ -22,24 +22,32 @@ raw_data <- labelled::to_factor(raw_data)
 # this depending on your interests)
 names(raw_data)
 
+#selecting variables of interest
 reduced_data <- 
   raw_data %>% 
-  select(region,
-         stateicp,
-         sex, 
-         age, 
-         race, 
-         hispan,
-         marst, 
-         bpl,
-         citizen,
+  select(age,
+         race,
+         hcovany,
+         educ,
          educd,
-         labforce,
-         inctot)
+         schltype,
+         vetstat)
+
+
+#### What's next? ####
+#grouping the ages
+reduced_data <- reduced_data %>% 
+  mutate(age_range = if_else(1 <= as.numeric(age) & as.numeric(age) <= 17,'0-17 years old', 
+                    if_else(18 <= as.numeric(age) & as.numeric(age) <= 30, '18-30 years old', 
+                            if_else(31 <= as.numeric(age) & as.numeric(age) <= 45, '31-45 years old',
+                                    if_else(46 <= as.numeric(age) & as.numeric(age) <= 64, '46-64 years old',
+                                            if_else(65<= as.numeric(age), '65+ years old', 'less than 1 year old'))))))
+                                          
+  
 rm(raw_data)
          
 
-#### What's next? ####
+
 
 
 
